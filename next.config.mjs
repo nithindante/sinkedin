901cb2eb86a4b6a2feed3b964fe7b9ed77f7aaf8
Preet Sojitra <preet.dev373@gmail.com>
2025-06-23 01:03:38 +0530

    [fix-deploy]: update next config to handle the supabase storage images url

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   images: {
     remotePatterns: [
+      new URL(
+        "https://pbelkncykqidzqofiyph.supabase.co/storage/v1/object/public/avatars/**"
+      ),
       new URL("http://127.0.0.1:54321/storage/v1/object/public/avatars/**"),
     ],
   },

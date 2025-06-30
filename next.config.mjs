cc049947c5468c838cc53fed6cc763ca9bd67b99
Preet Sojitra <preet.dev373@gmail.com>
2025-06-30 23:22:33 +0530

    ready for production launch

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,8 @@ const nextConfig = {
   images: {
     remotePatterns: [
       new URL(
-        "https://pbelkncykqidzqofiyph.supabase.co/storage/v1/object/public/avatars/**"
+        "https://qshaswfrhqefydyetqvq.supabase.co/storage/v1/object/public/avatars/**"
       ),
-      new URL("http://127.0.0.1:54321/storage/v1/object/public/avatars/**"),
     ],
   },
 }
